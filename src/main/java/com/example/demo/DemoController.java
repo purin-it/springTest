a84f53c499d5fbc409fae1cc0c8734b4089fcf29
purin-it <stanahas@amber.plala.or.jp>
2020-11-08 11:11:44 +0900

    modified

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,13 +1,15 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DemoController {
 
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("message", "こちらはspringTestアプリケーションです。");
 		return "index";
 	}
 	
